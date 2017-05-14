61c1558e008a0d3e03a0dc48a83b47bbcbccf354
Simon Podhajsky <simon.podhajsky@gmail.com>
2017-05-14 00:10:31 -0400

    Add useful quick-setting / navigation commands

--- rules/navigation.py
+++ rules/navigation.py
@@ -36,7 +36,11 @@ class NavigationRule(MappingRule):
         # Tabs:
         "[<n>] tab right": Key("g, t"),
         "[<n>] tab left": Key("g, T"),
-        "tab new": Key("colon") + Text("tabnew") + Key("enter"),
+
+        # New:
+        "new tab": Key("colon") + Text("tabnew") + Key("enter"),
+        "new split": Key("colon") + Text("split") + Key("enter"),
+        "new fault": Key("colon") + Text("vsplit") + Key("enter"),
     }
     extras = [
         IntegerRef("n", 1, 10),
--- rules/quick_settings.py
+++ rules/quick_settings.py
@@ -1,4 +1,4 @@
-from dragonfly import MappingRule, Key, Text
+from dragonfly import MappingRule, Key, Text, Dictation
 
 class QuickSettingsRule(MappingRule):
     mapping = {
@@ -7,4 +7,34 @@ class QuickSettingsRule(MappingRule):
 
         "win scroll": Key("colon") + Text("windo scrollbind"),
         "win scroll off": Key("colon") + Text("windo noscrollbind"),
+
+        "fix exec": Key("q, colon"),
+        "fix search": Key("q, slash"),
+
+        "show registers": Text(":registers") + Key("enter"),
+        "show dir": Text(":pwd") + Key("enter"),
+        "show history": Text(":history") + Key("enter"),
+        "show buffers": Text(":buffers") + Key("enter"),
+        "show version": Text(":version") + Key("enter"),
+
+        "show help": Text(":help ") + Key("enter"),
+
+        # Preview windows:
+        "show quick fix": Text(":copen") + Key("enter"),
+        "hide quick fix": Text(":cclose") + Key("enter"),
+
+        "show lock": Text(":lopen") + Key("enter"),
+        "hide lock": Text(":lclose") + Key("enter"),
+
+        # GUI windows:
+        "system search": Text(":promptfind") + Key("enter"),
+        "system sub": Text(":promptrepl") + Key("enter"),
+
+        "show help [<text>]": Key("colon, h, space") + Text("%(text)s"),
+    }
+    extras = [
+        Dictation('text')
+    ]
+    defaults = {
+        'text': ''
     }
